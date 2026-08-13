2df4ad4171f7788a2ca07a249aeb697c1ac0e4d0
yair-arm <armandoy11007@gmail.com>
2026-08-13 11:29:07 -0500

    Agrega cabecera de InterfazConsola

---
+++ include/InterfazConsola.h
@@ -0,0 +1,28 @@
+#ifndef INTERFAZCONSOLA_H
+#define INTERFAZCONSOLA_H
+
+#include <string>
+#include "GestorJSON.h"
+
+class Aerolinea;
+class Avion;
+
+class InterfazConsola {
+private:
+    GestorJSON gestor;
+    Aerolinea* aerolineas[3];
+    Avion** aviones;
+    int numAviones;
+
+    void mostrarItinerarios();
+    void modificarVuelo();
+    void cancelarVuelo();
+    Avion* buscarAvion(std::string id) const;
+
+public:
+    InterfazConsola();
+    ~InterfazConsola();
+    void iniciarMenu();
+};
+
+#endif
